2553e47fc8e204183cda198bbcb4dcea9e58cc78
Alex Schittko <alex4108@live.com>
2021-02-25 22:38:59 -0600

    Tell travis env variables for bot.py

--- scripts/1-test.sh
+++ scripts/1-test.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 cd ${TRAVIS_BUILD_DIR}
 pip3 install -r requirements.txt
 cd ./src
+
+export APPROOVA_DB_PATH="${TRAVIS_BUILD_DIR}/sqlite.db"
+export APPROOVA_DOTENV_PATH="${TRAVIS_BUILD_DIR}/src/.env"
+
 python3 bot.py
