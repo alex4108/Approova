6af50b18f60b1479e746069a5f89ca774a00868a
Alex Schittko <alex4108@live.com>
2021-02-26 02:08:26 -0600

    More travis fixing?

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,16 @@
 # Release RELEASE_VERSION
 
+## Breaking Changes
+
+* None!
+
 ## Bugs
 
 * Fixed `is_owner()` to perform the check properly.
 
 ## Improvements
 
-* Docker support for `linux/arm64/v8`
+* Docker support for `linux/arm64/v8` (aarch64)
 * Kubernetes templates
 * Automated deployment for public bot
 * Automated releases to github
--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -9,6 +9,8 @@ version=$(cat ${TRAVIS_BUILD_DIR}/VERSION)
 # Configures git for GPG Signing
 gitConfig() { 
     gpg --import ${TRAVIS_BUILD_DIR}/travis.gpg
+    mkdir -p ~/.ssh
+    cmhod 700 ~/.ssh
     mv ${TRAVIS_BUILD_DIR}/id_rsa ~/.ssh/id_rsa
     chmod 400 ~/.ssh/id_rsa
     git config --local user.name "Alex Schittko"
@@ -17,20 +19,30 @@ gitConfig() {
     echo -e "Host github.com\n    StrictHostKeyChecking no" > ~/.ssh/config
 }
 
-# Resets files between releases
-reset() { 
-    echo -e "# Release RELEASE_VERSION\n\n## Bugs\n\n*\n\n## Improvements\n\n*\n""" > CHANGELOG.md
+# Bumps the version
+bumpVersion() { 
+    echo -e "# Release RELEASE_VERSION\n\n## Breaking Changes\n\n*\n\n## Bugs\n\n*\n\n## Improvements\n\n*\n""" > CHANGELOG.md
     next_version_minor="$(( $(cat ${TRAVIS_BUILD_DIR}/VERSION | cut -d. -f3) + 1 ))"
-    next_version="$(cat ${TRAVIS_BUILD_DIR}/VERSION | cut -d. -f1).$(cat ${TRAVIS_BUILD_DIR}/VERSION | cut -d. -f2).${next_version_minor}"
-    OLDPWD=$(pwd)
-    mkdir /tmp/
-    cd /tmp/
-    git clone git@github.com:alex4108/approova.git
+    next_version="$(cat ${TRAVIS_BUILD_DIR}/VERSION | cut -d. -f1).$(cat ${TRAVIS_BUILD_DIR}/VERSION | cut -d. -f2).${next_version_minor}" > VERSION
     git checkout develop
+    rm -rf VERSION
+    rm -rf CHANGELOG.md
+    cp ${TRAVIS_BUILD_DIR}/CHANGELOG.md ./CHANGELOG.md
+    cp ${TRAVIS_BUILD_DIR}/VERSION ./VERSION
     git add CHANGELOG.md
     git add VERSION
     git commit -S -m "Reset VERSION & CHANGELOG.md"
     git push
+}
+
+# Resets files between releases
+reset() { 
+    OLDPWD=$(pwd)
+    mkdir /tmp/
+    cd /tmp/
+    git clone git@github.com:alex4108/approova.git
+    cd approova
+    bumpVersion
     cd ${OLDPWD}
 }
 
@@ -50,7 +62,7 @@ elif [[ "${STATE}" == "AFTER" ]]; then
     changelog=$(cat ${TRAVIS_BUILD_DIR}/CHANGELOG.md | sed "0,/RELEASE_VERSION/{s/RELEASE_VERSION/${version}/}")
 
     curl -X PATCH https://api.github.com/repos/alex4108/approova/releases/${last_release_id} -u alex4108:${GITHUB_PAT} -d "{\"name\": \"v${version}\", \"body\": \"${changelog}\"}"
-    #curl -X PATCH https://api.github.com/repos/alex4108/approova/releases/${last_release_id} -u alex4108:${GITHUB_PAT} -d "{\"draft\": \"false\"}"
+    # curl -X PATCH https://api.github.com/repos/alex4108/approova/releases/${last_release_id} -u alex4108:${GITHUB_PAT} -d "{\"draft\": \"false\"}"
 
     reset
 fi
